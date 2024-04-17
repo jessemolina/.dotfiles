e98f49be35609b4c260bc7a3f3d78933a6e39065
Jesse Molina <jesse@jessemolina.xyz>
2024-04-16 21:11:14 -0500

    Reorganized init.

--- nvim/.config/nvim/lua/plugins/tools/init.lua
+++ nvim/.config/nvim/lua/plugins/tools/init.lua
@@ -1,8 +1,10 @@
 return {
-  require("plugins.tools.neogit"),              -- Interactive git interface
-  require("plugins.tools.obsidian"),            -- Zettelkasten note taking
+  require("plugins.tools.molten"),             -- Interactive git interface
+  require("plugins.tools.neogit"),             -- Interactive git interface
+  require("plugins.tools.obsidian"),           -- Zettelkasten note taking
+  require("plugins.tools.pomo"),               -- Pomodoro Timer
   require("plugins.tools.telescope-ui-select"), -- Set vim ui select to telescope
-  require("plugins.tools.telescope"),           -- Highly extendable fuzzy finder
-  require("plugins.tools.vim-dadbod-ui"),       -- Database management interface
-  require("plugins.tools.which-key")            -- Display available keybinds per initialized key commands
+  require("plugins.tools.telescope"),          -- Highly extendable fuzzy finder
+  require("plugins.tools.vim-dadbod-ui"),      -- Database management user interface
+  require("plugins.tools.which-key"),          -- Display available keybinds per initialized key commands
 }
